54848c2f20fd01b13e82be5760d5fad3dfca0c8e
Skilldus <anthony.leguyader@etudiant.univ-rennes1.fr>
2021-02-20 11:13:20 +0100

    Dynamic content component (il ne marche pas je galère là)

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
     <Navbar></Navbar>
+    <Content canvas="./components/navbar"></Content>
   </div>
 </template>
 
 <script>
 import Navbar from "./components/Navbar.vue";
+import Content from "./components/Content.vue"
 
 export default {
   name: "App",
   components: {
-    Navbar
+    Navbar,
+    Content
   }
 };
 </script>
---
+++ src/components/Content.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="content" class="container" v-bind:is="readContent">
+    {{ content }}
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "Content",
+  props: ['content'],
+  computed: {
+    readContent() {
+      let data = this.canvas.split('/')[2];
+      return () => import(`./components/${data}`);
+    }
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
