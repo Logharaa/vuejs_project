c457bb040a06d408c5d66e2baa0ddf7788644e0b
Skilldus <anthony.leguyader@etudiant.univ-rennes1.fr>
2021-02-20 23:28:39 +0100

    Recherche components + import sortTable

---
+++ src/components/Element.vue
@@ -0,0 +1,19 @@
+<template>
+
+</template>
+
+<script>
+
+export default {
+  name: "Element",
+  data () {
+    return {
+      
+    }
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Table.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="maRecherche">
+    <h1>Recherche</h1>
+    <Element></Element>
+  </div>
+</template>
+
+<script>
+import Element from "./Element.vue";
+import { sortTable } from '../assets/data.min.js'
+
+export default {
+  name: "Table",
+  components: {
+    Element
+  },
+  data () {
+    return {
+      sorts: sortTable
+    }
+  }
+};
+</script>
+
+<style scoped>
+  h1 {
+    color: white;
+  }
+</style>
